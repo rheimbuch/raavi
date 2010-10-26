4d96c04b6ff9401f1cae1d2433ea4822b1bd5ea0
Ryan Heimbuch <rheimbuch@gmail.com>
2010-10-25 21:08:03 -0600

    require in java libs/jars

--- lib/raavi.rb
+++ lib/raavi.rb
@@ -1,3 +1,7 @@
+JAR_LIB = File.expand_path(File.join(File.dirname(__FILE__), 'java'))
+Dir[File.join(JAR_LIB,'*.jar')].each {|jar| require jar}
+
+require 'java'
 module Raavi
   # Your code goes here...
 end
